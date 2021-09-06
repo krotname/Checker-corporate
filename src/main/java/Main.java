3a6a47a37e62be6f084d33926e946113a3903ff0
Овчаренко Андрей <ia@krot.name>
2021-09-06 13:14:20 +0300

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         System.out.println(CheckerCorporate.isActive("9710083390")); // true активна
         System.out.println(CheckerCorporate.isActive("9710083391")); // false не существует
