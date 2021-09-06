49ec66f1f10e20cc54dd06b22b1c8f632a23b1e8
Овчаренко Андрей <ia@krot.name>
2021-09-06 13:24:13 +0300

    upd

--- src/main/java/CheckerCorporate.java
+++ src/main/java/CheckerCorporate.java
@@ -12,6 +12,8 @@ import java.util.Objects;
 import java.util.Properties;
 
 public class CheckerCorporate {
+    private static final String URL = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/party";
+
     public static boolean isActive(String INN) {
         if (INN == null || INN.length() < 8 || INN.length() > 12) {
             return false;
@@ -23,7 +25,7 @@ public class CheckerCorporate {
             appProps.load(new FileInputStream(appConfigPath));
             String token = appProps.getProperty("token");
 
-            Content postResultForm = Request.Post("https://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/party")
+            Content postResultForm = Request.Post(URL)
                     .bodyString("{ \"query\": \"" + INN + "\" }", ContentType.APPLICATION_JSON)
                     .setHeader("Authorization", "Token " + token)
                     .setHeader("Content-Type", "application/json; charset=UTF-8")
